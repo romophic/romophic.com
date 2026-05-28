e3eedbcb6e64f736d421babf87154e9d6b5357b0
romophic <cockatiel.u10@gmail.com>
2026-05-28 13:00:07 +0900

    chore: fix eslint any types in test files

--- src/components/BlogCard.test.ts
+++ src/components/BlogCard.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { describe, it, expect } from 'vitest'
 import { experimental_AstroContainer as AstroContainer } from 'astro/container'
 import BlogCard from './BlogCard.astro'
--- src/pages/_llms.txt.test.ts
+++ src/pages/_llms.txt.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { describe, it, expect } from 'vitest'
 import { GET } from './llms.txt'
 import { SITE } from '@/consts'
